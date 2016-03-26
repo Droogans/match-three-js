c059e4125b24a2ec3ebcb2cad6266af1eb76492f
rbcasperson <casperson.ryan@gmail.com>
2016-03-26 12:02:48 -0400

    fixes evaluate test & implements incoming orbs option A `dropOptions` parameter was added to `evaluate()` which defaults to `this.types` This option helps with the testing.

--- babel/test/data/evaluate.js
+++ babel/test/data/evaluate.js
@@ -35,7 +35,7 @@ export var  testMatchData = [
     ]
 ];
 
-// data 'nonmatch orbs drop down into the correct place'
+// data for 'nonmatch orbs drop down into the correct place'
 //   -> [[row, sliceStart, sliceEnd], [droppedOrbs]]
 export var nonMatchDropped = [
     [
@@ -97,6 +97,63 @@ export var nonMatchDropped = [
         [[2, 3, 5], [3, 4]],
         [[1, 0, 2], [1, 2]],
         [[1, 3, 5], [4, 5]]
+    ]
+];
+
+// data for 'orbs from dropOptions fill in the rest of the board'
+//   -> [[row, sliceStart, sliceEnd]] -> each should be included in dropOptions
+export var droppedRandoms = [
+    [
+        [0, 1, 4]
+    ],
+    [
+        [2, 4, 5],
+        [1, 4, 5],
+        [0, 4, 5]
+    ],
+    [
+        [0, 1, 5]
+    ],
+    [
+        [3, 3, 4],
+        [2, 3, 4],
+        [1, 3, 4],
+        [0, 3, 4]
+    ],
+    [
+        [0, 0, 5]
+    ],
+    [
+        [4, 2, 3],
+        [3, 2, 3],
+        [2, 2, 3],
+        [1, 2, 3],
+        [0, 2, 3]
+    ],
+    [
+        [2, 2, 3],
+        [1, 2, 3],
+        [0, 1, 4]
+    ],
+    [
+        [2, 1, 2],
+        [1, 1, 2],
+        [0, 1, 4]
+    ],
+    [
+        [2, 2, 3],
+        [1, 2, 3],
+        [0, 0, 4]
+    ],
+    [
+        [2, 2, 3],
+        [1, 2, 3],
+        [0, 1, 5]
+    ],
+    [
+        [2, 2, 3],
+        [1, 2, 3],
+        [0, 0, 5]
     ]
 ];
 
--- babel/test/evaluate.js
+++ babel/test/evaluate.js
@@ -5,39 +5,63 @@ import {orbs} from './data/orbs';
 import {combinedMatches} from './data/combinedMatches';
 import {testMatchData} from './data/evaluate';
 import {nonMatchDropped} from './data/evaluate';
+import {droppedRandoms} from './data/evaluate';
 import {evaluate} from '../src/board';
 import {findMatches} from '../src/board';
 import {combineMatches} from '../src/board';
 import {Board} from '../src/board';
-let board = new Board(5, 5);
-let index;
+let board;
+let evaluatedOrbs;
+let matchDatas;
 let name;
 
-_.each(_.range(orbs.length), i => {
-    name = names[i];
-    board.orbs = orbs[i];
-    let matchData = board.evaluate(combineMatches(findMatches(board.orbs)));
+test.before(() => {
+    board = new Board(5, 5);
+    evaluatedOrbs = [];
+    matchDatas = [];
+    
+    _.each(_.range(orbs.length), i => {
+        board.orbs = orbs[i];
+        let matchData = board.evaluate(combineMatches(findMatches(board.orbs)), [7, 8]);
+        evaluatedOrbs.push(board.orbs);
+        matchDatas.push(matchData);
+    });  
+});
 
+_.each(_.range(orbs.length), j => {
+    name = names[j];
+    
     test(`gathers data for ${name}`, t => {
-        t.ok(_.isEqual(matchData, testMatchData[i]));
+        t.ok(_.isEqual(matchDatas[j], testMatchData[j]));
     });
 
     test(`removes matches and replaces with valid type orbs for ${name}`, t => {
-        _.each(_.flattenDeep(board.orbs), orb => {
-            t.true(_.includes(board.types, orb))
+        _.each(_.flattenDeep(evaluatedOrbs[j]), orb => {
+            t.true(_.includes(_.range(9), orb));
         });
     });
 
     test(`nonmatch orbs drop down into the correct place for ${name}`, t => {
-        board.orbs = orbs[i];
-        board.evaluate(combineMatches(findMatches(board.orbs)));
-    
-        let dropped = nonMatchDropped[i];
+        let dropped = nonMatchDropped[j];
         _.each(dropped, section => {
             let [sliceData, droppedOrbs] = section;
             let [row, start, end] = sliceData;
-            t.ok(_.isEqual(_.slice(board.orbs[row], start, end), droppedOrbs));
+            t.ok(_.isEqual(_.slice(evaluatedOrbs[j][row], start, end), droppedOrbs));
         });
+    });
+    
+    test(`orbs from dropOptions fill in the rest of the board for ${name}`, t => {
+        let randoms = droppedRandoms[j];
+        _.each(randoms, sliceData => {
+            let [row, start, end] = sliceData;
+            _.each(_.slice(evaluatedOrbs[j][row], start, end), orb => {
+                t.true(_.includes([7, 8], orb));    
+            });
+        });
+    });
+    
+    test.skip(`unaffected orbs are unchanged for ${name}`, t => {
+        
     });
 });
 
